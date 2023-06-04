0c162d3b9d2e213d30cd775729a954b90d5d299b
Magomed Kostoev <mkostoevr@yandex.ru>
2023-06-04 09:55:02 +0300

    [REF] Move generator into single file.

--- gen.py
+++ gen.py
@@ -1,26 +1,406 @@
-from constants import *
-from utils import *
-from CGen import *
+# Constants ####################################################################
 
-def mnemonic_parts(mnemonic):
-    # Commas sould be followed by a space.
-    assert(mnemonic.count(',') == mnemonic.count(', '))
-    # Only single spaces allowed.
-    assert('  ' not in mnemonic)
-    # 'MOVZX reg32, reg8' -> [ 'MOVZX reg32', 'reg8' ]
-    comma_splitted = mnemonic.split(', ')
-    # 'MOVZX reg32' -> [ 'MOVZX', 'reg32' ]
-    name_and_possibly_first_operand = comma_splitted[0].split(' ')
-    # 'MOVZX'
-    name = name_and_possibly_first_operand[0]
-    # [ 'reg32', 'reg8' ]
-    operands = name_and_possibly_first_operand[1:] + comma_splitted[1:]
-    return name, tuple(operands)
+CGEN_PREFIX = 'X86GEN'
+CGEN_OUTPUT_COMPONENT_SEPARATOR = ',\n    '
+CGEN_PARAMETER_SEPARATOR = ',\n  '
 
-def get_opcode_parts(opcode):
-    # Only single spaces allowed.
-    assert('  ' not in opcode)
-    return tuple(opcode.split(' '))
+ARCH_I386 = 'i386'
+ARCH_AMD64 = 'amd64'
+
+arch_variants = (ARCH_I386, ARCH_AMD64)
+
+imms = {
+    'imm8': 8,
+    'imm16': 16,
+    'imm32': 32,
+    'imm64': 64,
+}
+
+regs = {
+    'reg8': 8,
+    'reg16': 16,
+    'reg32': 32,
+    'reg64': 64,
+}
+
+regmems = {
+    'reg/mem8': (8, 8),
+    'reg/mem16': (16, 16),
+    'reg/mem32': (32, 32),
+    'reg/mem64': (64, 64),
+    'reg16/mem16': (16, 16),
+    'reg32/mem16': (32, 16),
+    'reg64/mem16': (64, 16),
+}
+
+moffsets = {
+    'moffset8': 8,
+    'moffset16': 16,
+    'moffset32': 32,
+    'moffset64': 64,
+}
+
+modrms = {
+    '/r': None,
+    '/0': 0,
+    '/1': 1,
+    '/2': 2,
+    '/3': 3,
+    '/4': 4,
+    '/5': 5,
+    '/6': 6,
+    '/7': 7,
+}
+
+is_ = {
+    'ib': 8,
+    'iw': 16,
+    'id': 32,
+    'iq': 64,
+}
+
+plusrs = {
+    '+rb': 8,
+    '+rw': 16,
+    '+rd': 32,
+    '+rq': 64,
+}
+
+specifiedregs = {
+    'AL': 8,
+    'AX': 16,
+    'EAX': 32,
+    'RAX': 64,
+}
+
+segregs = {
+    'DS': 16,
+    'ES': 16,
+    'FS': 16,
+    'GS': 16,
+    'SS': 16,
+}
+
+B00 = 0
+B01 = 1
+B10 = 2
+B11 = 3
+
+B000 = 0
+B001 = 1
+B010 = 2
+B011 = 3
+B100 = 4
+B101 = 5
+B110 = 6
+B111 = 7
+
+# Utils ########################################################################
+
+def get_native_size(arch):
+    if arch == ARCH_I386:
+        return 32
+    elif arch == ARCH_AMD64:
+        return 64
+    else:
+        raise Exception('Error: Unknown architecture: %s' % (arch,))
+
+def is_tuple(obj):
+    return isinstance(obj, tuple)
+
+def is_int(obj):
+    return isinstance(obj, int)
+
+def is_str(obj):
+    return isinstance(obj, str)
+
+def is_hex_byte(x):
+    assert(is_str(x))
+    hexdigits = '0123456789abcdefABCDEF'
+    return len(x) == 2 and all(c in hexdigits for c in x)
+
+def is_byte_value(x):
+    return x >= 0 and x <= 255
+
+def is_reg_size(x):
+    return x in (8, 16, 32, 64)
+
+def is_imm_size(x):
+    return x in (8, 16, 32, 64)
+
+def is_8_to_64(x):
+    return x in (8, 16, 32, 64)
+
+def is_disp_size(x):
+    return x in (8, 32)
+
+def is_moffset_size(x):
+    return x in (32, 64)
+
+def is_native_size(x):
+    return x in (32, 64)
+
+def is_arch(x):
+    return x in arch_variants
+
+def without_nones(collection):
+    result = []
+    for it in collection:
+        if it != None:
+            result.append(it)
+    return tuple(result)
+
+def size_to_word(size):
+    if size == 8:
+        return 'byte'
+    elif size == 16:
+        return 'word'
+    elif size == 32:
+        return 'dword'
+    elif size == 64:
+        return 'qword'
+    else:
+        raise Exception('Unknown size: %d' % (size,))
+
+# CGen #########################################################################
+
+def is_cgen_parameter(x):
+    return isinstance(x, CGen_Parameter)
+
+class CGen_Output_Component:
+    pass
+
+class CGen_Output_Component_ModRm(CGen_Output_Component):
+    def __init__(self, mod, reg, rm):
+        assert(is_int(mod))
+        if reg != None:
+            assert(is_int(reg))
+            assert(reg in (0, 1, 2, 3, 4, 5, 6, 7))
+        if rm != None:
+            assert(is_int(rm))
+            assert(rm in (0, 1, 2, 3, 4, 5, 6, 7))
+        self.mod = '%s_B%s' % (CGEN_PREFIX, format(mod, '02b'),)
+        self.reg = 'reg' if reg == None else '%s_B%s' % (
+            CGEN_PREFIX,
+            format(reg, '03b'),
+        )
+        self.rm = 'rm_reg' if rm == None else '%s_B%s' % (
+            CGEN_PREFIX,
+            format(rm, '03b'),
+        )
+
+    def emit(self):
+        return '%s_MOD_RM(%s, %s, %s)' % (CGEN_PREFIX, self.mod, self.reg, self.rm,)
+
+class CGen_Output_Component_ModRm_Reg(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B11, reg_value, None)
+
+class CGen_Output_Component_ModRm_AtReg(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B00, reg_value, None)
+
+class CGen_Output_Component_ModRm_AtDisp32(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B00, reg_value, B101)
+
+class CGen_Output_Component_ModRm_AtRegPlusDisp8(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B01, reg_value, None)
+
+class CGen_Output_Component_ModRm_AtRegPlusDisp32(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B10, reg_value, None)
+
+class CGen_Output_Component_ModRm_AtScaleIndexBase(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B00, reg_value, B100)
+
+class CGen_Output_Component_ModRm_AtScaleIndexBaseDisp8(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B01, reg_value, B100)
+
+class CGen_Output_Component_ModRm_AtScaleIndexBaseDisp32(CGen_Output_Component_ModRm):
+    def __init__(self, reg_value):
+        super().__init__(B10, reg_value, B100)
+
+class CGen_Output_Component_Sib(CGen_Output_Component):
+    def emit(self):
+        return '%s_SIB(sib_scale, sib_index, sib_base)' % (CGEN_PREFIX,)
+
+class CGen_Output_Component_Value(CGen_Output_Component):
+    def __init__(self, name, size):
+        assert(is_str(name))
+        assert(is_int(size))
+        assert(is_8_to_64(size))
+        self.name = name
+        self.size = size
+
+    def emit(self):
+        if self.size == 8:
+            return '%s' % (self.name)
+        elif self.size in (16, 32, 64):
+            return '%s_EXPAND_%d(%s)' % (CGEN_PREFIX, self.size, self.name)
+        else:
+            raise Exception('Unknown size: %d' % (self.size,))
+
+class CGen_Output_Component_Disp(CGen_Output_Component_Value):
+    def __init__(self, size):
+        super().__init__('disp', size)
+
+class CGen_Output_Component_Imm(CGen_Output_Component_Value):
+    def __init__(self, size):
+        super().__init__('imm', size)
+
+class CGen_Output_Component_Moffset(CGen_Output_Component_Value):
+    def __init__(self, size):
+        super().__init__('moffset', size)
+
+class CGen_Output_Component_Byte(CGen_Output_Component):
+    def __init__(self, value):
+        assert(is_int(value))
+        assert(is_byte_value(value))
+        self.value = value
+
+    def emit(self):
+        return '0x%02X' % (self.value,)
+
+class CGen_Output_Component_BytePlusReg(CGen_Output_Component):
+    def __init__(self, value):
+        assert(is_int(value))
+        assert(is_byte_value(value))
+        self.value = value
+
+    def emit(self):
+        return '0x%02X + reg' % (self.value,)
+
+class CGen_Output_Component_Prefix_OperandSizeOverride(CGen_Output_Component):
+    def emit(self):
+        return '%s_PREFIX_OPERAND_SIZE_OVERRIDE' % (CGEN_PREFIX,)
+
+class CGen_Output_Component_Prefix_RexW(CGen_Output_Component):
+    def emit(self):
+        return '%s_PREFIX_REX_W' % (CGEN_PREFIX,)
+
+class CGen_Output:
+    def __init__(self, components):
+        self.components = components
+
+    def emit(self):
+        return CGEN_OUTPUT_COMPONENT_SEPARATOR.join(
+            [component.emit() for component in self.components]
+        )
+
+class CGen_Parameter:
+    pass
+
+class CGen_Parameter_Imm(CGen_Parameter):
+    def __init__(self, size):
+        assert(is_int(size))
+        assert(is_imm_size(size))
+        self.size = size
+
+    def emit(self):
+        return 'uint%d_t imm' % (self.size,)
+
+class CGen_Parameter_Moffset(CGen_Parameter):
+    def __init__(self, size):
+        assert(is_int(size))
+        assert(is_moffset_size(size))
+        self.size = size
+
+    def emit(self):
+        return 'uint%d_t moffset' % (self.size,)
+
+class CGen_Parameter_Disp(CGen_Parameter):
+    def __init__(self, size):
+        assert(is_int(size))
+        assert(is_disp_size(size))
+        self.size = size
+
+    def emit(self):
+        return 'uint%d_t disp' % (self.size,)
+
+class CGen_Parameter_Reg(CGen_Parameter):
+    def __init__(self, size):
+        assert(is_int(size))
+        assert(is_reg_size(size))
+        self.size = size
+
+    def emit(self):
+        return 'enum X86Gen_Reg%d reg' % (self.size,)
+
+class CGen_Parameter_SegReg(CGen_Parameter):
+    def emit(self):
+        return 'enum X86Gen_SegReg segReg'
+
+class CGen_Parameter_RmReg(CGen_Parameter):
+    def __init__(self, size):
+        assert(is_int(size))
+        assert(is_reg_size(size))
+        self.size = size
+
+    def emit(self):
+        return 'enum X86Gen_Reg%d rm_reg' % (self.size,)
+
+class CGen_Parameter_Scale(CGen_Parameter):
+    def emit(self):
+        return 'enum X86Gen_Scale sib_scale'
+
+class CGen_Parameter_Index(CGen_Parameter):
+    def __init__(self, size):
+        assert(is_int(size))
+        assert(is_native_size(size))
+        self.size = size
+
+    def emit(self):
+        return 'enum X86Gen_Reg%d sib_index' % (self.size,)
+
+class CGen_Parameter_Base(CGen_Parameter):
+    def __init__(self, size):
+        assert(is_int(size))
+        assert(is_native_size(size))
+        self.size = size
+
+    def emit(self):
+        return 'enum X86Gen_Reg%d sib_base' % (self.size,)
+
+class CGen_Parameter_Output(CGen_Parameter):
+    def emit(self):
+        return 'X86Gen_Output output'
+
+class CGen_Parameters:
+    def __init__(self, parameters):
+        assert(is_tuple(parameters))
+        for parameter in parameters:
+            assert(is_cgen_parameter(parameter))
+        # Each function unconditionally has the 'output' parameter.
+        self.parameters = (CGen_Parameter_Output(),) + parameters
+
+    def emit(self):
+        return CGEN_PARAMETER_SEPARATOR.join(
+            [parameter.emit() for parameter in self.parameters]
+        )
+
+class CGen_Function:
+    def __init__(self, comment, name, parameters, output):
+        self.comment = comment
+        self.name = name
+        self.parameters = parameters
+        self.output = output
+
+    def emit(self):
+        return (
+            '%s\n' +
+            'static void x86gen_%s(\n' +
+            '  %s\n' +
+            ') {\n' +
+            '  const uint8_t buf[] = {\n' +
+            '    %s\n' +
+            '  };\n' +
+            '  X86Gen_Output_Write(output, sizeof(buf), buf);\n' +
+            '}'
+        ) % (self.comment, self.name, self.parameters.emit(), self.output.emit())
 
 # Operand ######################################################################
 
@@ -56,9 +436,10 @@ class Operand_Moffset(Operand):
         )
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_Moffset(self.size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_Moffset(self.size, addr_size)
 
     def c_parameters(self):
         assert(self.address_size != None)
@@ -128,9 +509,10 @@ class Operand_RegMem_AtReg(Operand):
         )
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_RegMem_AtReg(self.mem_size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_RegMem_AtReg(self.mem_size, addr_size)
 
     def warning(self):
         return 'rm_reg can\'t be rSP or rBP.'
@@ -152,9 +534,10 @@ class Operand_RegMem_AtDisp32(Operand):
         return '%s[disp32]' % (size_to_word(self.mem_size),)
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_RegMem_AtDisp32(self.mem_size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_RegMem_AtDisp32(self.mem_size, addr_size)
 
     def c_parameters(self):
         assert(self.address_size != None)
@@ -176,9 +559,10 @@ class Operand_RegMem_AtRegPlusDisp8(Operand):
         )
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_RegMem_AtRegPlusDisp8(self.mem_size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_RegMem_AtRegPlusDisp8(self.mem_size, addr_size)
 
     def warning(self):
         return 'rm_reg can\'t be rSP.'
@@ -206,9 +590,10 @@ class Operand_RegMem_AtRegPlusDisp32(Operand):
         )
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_RegMem_AtRegPlusDisp32(self.mem_size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_RegMem_AtRegPlusDisp32(self.mem_size, addr_size)
 
     def warning(self):
         return 'rm_reg can\'t be rSP.'
@@ -237,9 +622,10 @@ class Operand_RegMem_AtScaleIndexBase(Operand):
         )
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_RegMem_AtScaleIndexBase(self.mem_size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_RegMem_AtScaleIndexBase(self.mem_size, addr_size)
 
     def c_parameters(self):
         assert(self.address_size != None)
@@ -266,9 +652,10 @@ class Operand_RegMem_AtScaleIndexBaseDisp8(Operand):
         )
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_RegMem_AtScaleIndexBaseDisp8(self.mem_size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_RegMem_AtScaleIndexBaseDisp8(self.mem_size, addr_size)
 
     def c_parameters(self):
         assert(self.address_size != None)
@@ -296,9 +683,10 @@ class Operand_RegMem_AtScaleIndexBaseDisp32(Operand):
         )
 
     def arch_specific(self, arch):
-        if self.address_size == None:
-            addr_size = get_native_size(arch)
-            return Operand_RegMem_AtScaleIndexBaseDisp32(self.mem_size, addr_size)
+        # This should only be called when the operand isn't specialized yet.
+        assert(self.address_size == None)
+        addr_size = get_native_size(arch)
+        return Operand_RegMem_AtScaleIndexBaseDisp32(self.mem_size, addr_size)
 
     def c_parameters(self):
         assert(self.address_size != None)
@@ -529,6 +917,28 @@ class Opcode_Prefix_RexW:
 
     def c_output_components(self):
         return (CGen_Output_Component_Prefix_RexW(),)
+
+# Parsing ######################################################################
+
+def mnemonic_parts(mnemonic):
+    # Commas sould be followed by a space.
+    assert(mnemonic.count(',') == mnemonic.count(', '))
+    # Only single spaces allowed.
+    assert('  ' not in mnemonic)
+    # 'MOVZX reg32, reg8' -> [ 'MOVZX reg32', 'reg8' ]
+    comma_splitted = mnemonic.split(', ')
+    # 'MOVZX reg32' -> [ 'MOVZX', 'reg32' ]
+    name_and_possibly_first_operand = comma_splitted[0].split(' ')
+    # 'MOVZX'
+    name = name_and_possibly_first_operand[0]
+    # [ 'reg32', 'reg8' ]
+    operands = name_and_possibly_first_operand[1:] + comma_splitted[1:]
+    return name, tuple(operands)
+
+def get_opcode_parts(opcode):
+    # Only single spaces allowed.
+    assert('  ' not in opcode)
+    return tuple(opcode.split(' '))
 
 # The rest #####################################################################
 
@@ -794,4 +1204,5 @@ def main():
     for entry in entries:
         handle_entry(entry)
 
-main()
+if __name__ == '__main__':
+    main()
